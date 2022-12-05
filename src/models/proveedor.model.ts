2acf6cd6160ceb0f05d656a3402d53ac5dcab16e
alextorres7920 <programacionchimuelos@gmail.com>
2022-12-05 14:16:24 -0500

    Creacion de modelos y datasource

---
+++ src/models/proveedor.model.ts
@@ -0,0 +1,28 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Proveedor extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  razon_social: string;
+
+
+  constructor(data?: Partial<Proveedor>) {
+    super(data);
+  }
+}
+
+export interface ProveedorRelations {
+  // describe navigational properties here
+}
+
+export type ProveedorWithRelations = Proveedor & ProveedorRelations;
